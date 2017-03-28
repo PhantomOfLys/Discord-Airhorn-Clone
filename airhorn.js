6dbb2048d68228547891399621cad7ebd8c2955b
PhantomOfLys <lys.smol.rabbit@gmail.com>
2017-03-28 13:09:47 +0200

    Update airhorn.js

--- airhorn.js
+++ airhorn.js
@@ -14,19 +14,24 @@ var gPrefix = '!';
 client.on('message', message => {
   if (message.content.startsWith(gPrefix) && !(message.member.voiceChannel === undefined) && AinsleyVC === undefined && message.content.length > gPrefix.length)
   {
-    
     fs.readdir(message.content.substr(gPrefix.length), (err, audioFiles) => {
       if(audioFiles===undefined) return;
       var audioFile = message.content.substr(gPrefix.length) + '/' + audioFiles[Math.floor(Math.random()*audioFiles.length)];
       gAirhornVC = message.member.voiceChannel;
-      gAirhornVC.join().then(connection => {
-        gAirhornAS = connection;
-        gAirhornDS = gAirhornAS.playFile(audioFile);
-        gAirhornDS.on('end', reason => {
-          gAirhornVC.leave();
-          gAirhornVC = undefined;        
+      try{
+        gAirhornVC.join().then(connection => {
+          gAirhornAS = connection;
+          gAirhornDS = gAirhornAS.playFile(audioFile);
+          gAirhornDS.on('end', reason => {
+            try{ gAirhornVC.leave(); }catch(e){ console.log(e); }
+            gAirhornVC = undefined;        
+          });
         });
-      }); 
+      }catch(e)
+      {
+        console.log(e);
+        gAirhornVC = undefined; 
+      }
     });
   }
 });
